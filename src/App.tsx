fa5df3913aa3350fcd023ebb8269d95fb3b1b4fb
AyubovAsadbek <ayubovasadbek33@gmail.com>
2025-05-04 00:18:02 +0500

    dsdsds

--- src/App.tsx
+++ src/App.tsx
@@ -49,8 +49,12 @@ const App = () => {
   };
 
   const onCheckout = () => {
-    telegram.MainButton.text = "Sotib olish :)";
-    telegram.MainButton.show();
+    if (cartItems.length !== 0) {
+      telegram.MainButton.text = "Sotib olish :)";
+      telegram.MainButton.show();
+    } else {
+      telegram.MainButton.hide();
+    }
   };
 
   const onSendData = useCallback(() => {
